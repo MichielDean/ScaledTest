d2139f3d0ff1beed6584d056b72bb8928d85b3ae
Michiel Bugher <miyike@gmail.com>
2025-05-23 15:38:15 -0600

    Test pre-commit hook

---
+++ test-format.js
@@ -0,0 +1,13 @@
+// This is an intentionally unformatted file to test our pre-commit hook
+const test = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+function hello() {
+  console.log('Hello world!');
+  return 42;
+}
+
+hello();
